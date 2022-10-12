7712a7d4a28a4acc0ebdfe573c903dae3e63783f
Nikola Ivanovic <nikola.ivanovic@vegait.rs>
2022-10-12 23:17:08 +0200

    Added green section bg

--- lib/common/widgets/scrollable_content/scrollable_content_widget.dart
+++ lib/common/widgets/scrollable_content/scrollable_content_widget.dart
@@ -62,75 +62,105 @@ class ScrollableContentWidget extends StatelessWidget {
             top: backgroundOffset,
             child: Image.asset('images/about_us_bg.png', scale: 0.8),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 20),
-                _renderSubtitle(context),
-                Text(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: _renderSubtitle(context),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
                   title,
                   style: Theme.of(context).textTheme.headline2,
                 ),
-                const SizedBox(height: 68),
-                Text(
+              ),
+              const SizedBox(height: 68),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
                   content,
                   style: Theme.of(context).textTheme.bodyText1,
                 ),
-                const SizedBox(height: 35),
-                Text(
-                  'PODSTICARIJUMNE IGRE I AKTIVNOSTI, STIMULATIVNE ZA RAZVOJ VAŠE BEBE I DETETA',
-                  style: Theme.of(context).textTheme.headline2,
+              ),
+              const SizedBox(height: 35),
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/green_flag_section_bg.png'),
+                    fit: BoxFit.fill,
+                  ),
                 ),
-                const SizedBox(height: 12),
-                const FlagSection(
-                  content: _greenFlag1Content,
-                  flagType: FlagType.green,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Text(
+                        'PODSTICARIJUMNE IGRE I AKTIVNOSTI, STIMULATIVNE ZA RAZVOJ VAŠE BEBE I DETETA',
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                      const SizedBox(height: 12),
+                      const FlagSection(
+                        content: _greenFlag1Content,
+                        flagType: FlagType.green,
+                      ),
+                      const FlagSection(
+                        content: _greenFlag2Content,
+                        flagType: FlagType.green,
+                      ),
+                      const FlagSection(
+                        content: _greenFlag3Content,
+                        flagType: FlagType.green,
+                      ),
+                      const FlagSection(
+                        content: _greenFlag4Content,
+                        flagType: FlagType.green,
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
                 ),
-                const FlagSection(
-                  content: _greenFlag2Content,
-                  flagType: FlagType.green,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Text(
+                      'ZNAKOVI ODSTUPANJA OD NEUROTIPIČNOG RAZVOJA',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    const SizedBox(height: 12),
+                    const FlagSection(
+                      content: _redFlag1Content,
+                      flagType: FlagType.red,
+                    ),
+                    const SizedBox(height: 16),
+                    const FlagSection(
+                      content: _redFlag2Content,
+                      flagType: FlagType.red,
+                    ),
+                    const SizedBox(height: 54),
+                    const FlagSection(
+                      content: _redFlag3Content,
+                      flagType: FlagType.red,
+                    ),
+                    const SizedBox(height: 48),
+                    const FlagSection(
+                      content: _redFlag4Content,
+                      flagType: FlagType.red,
+                    ),
+                  ],
                 ),
-                const FlagSection(
-                  content: _greenFlag3Content,
-                  flagType: FlagType.green,
-                ),
-                const FlagSection(
-                  content: _greenFlag4Content,
-                  flagType: FlagType.green,
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  'ZNAKOVI ODSTUPANJA OD NEUROTIPIČNOG RAZVOJA',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-                const SizedBox(height: 12),
-                const FlagSection(
-                  content: _redFlag1Content,
-                  flagType: FlagType.red,
-                ),
-                const SizedBox(height: 16),
-                const FlagSection(
-                  content: _redFlag2Content,
-                  flagType: FlagType.red,
-                ),
-                const SizedBox(height: 54),
-                const FlagSection(
-                  content: _redFlag3Content,
-                  flagType: FlagType.red,
-                ),
-                const SizedBox(height: 48),
-                const FlagSection(
-                  content: _redFlag4Content,
-                  flagType: FlagType.red,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 22, bottom: 36),
-                  child: Image.asset('images/separator.png'),
-                )
-              ],
-            ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 22, bottom: 36),
+                child: Image.asset('images/separator.png'),
+              )
+            ],
           ),
         ],
       ),
