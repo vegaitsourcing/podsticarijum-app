fd61f7a3801f6f5b2b3594e77cf542a57c49498b
Mihailo Radevic <Mihailo.Radevic@r2conline.com>
2022-10-15 19:58:13 +0200

    backed initial commit

---
+++ podsticarijum_backend/IPodsticarijumMailService.cs
@@ -0,0 +1,13 @@
+﻿namespace podsticarijum_backend;
+
+public interface IPodsticarijumMailService
+{
+    Task sendEmail(string mailAddressTo);
+
+    public string? Body { get; set; }
+    public string? Subject { get; set; }
+    public string? FromName { get; }
+    public string? ToName { get; }
+    public string? AppPackageName { get; set; }
+}
+
