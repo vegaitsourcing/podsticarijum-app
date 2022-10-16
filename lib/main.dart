df5702509181ab4d6c2906721193bc484d1acf15
Nikola Ivanovic <nikola.ivanovic@vegait.rs>
2022-10-16 14:28:05 +0200

    Minor change

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const FamilyBackupApp());
-} 
+}
 
 class FamilyBackupApp extends StatelessWidget {
   const FamilyBackupApp({super.key});
