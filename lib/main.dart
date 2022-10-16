cca26b16e7839597d7cdf05f4171b3e0e1b848b2
Nikola Ivanovic <nikola.ivanovic@vegait.rs>
2022-10-16 14:52:24 +0200

    Post merge changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 import 'common/theme.dart';
 import 'screens/categories_screen/subcategories_screen.dart';
